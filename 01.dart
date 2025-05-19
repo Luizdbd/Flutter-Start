6ec8352fabe31fe143e4853d121b48ee3e1eaa53
Luizdbd <luizdbd45@gmail.com>
2025-05-19 19:47:12 -0400

    Add files via upload

--- 01.dart
+++ 01.dart
@@ -1,10 +1,49 @@
-void main() {
-  Map<String, int> idades = {
-    'Alice': 25,
-    'Bruno': 30,
-    'Carla': 28,
-  };
+import 'dart:io';
 
-  print(idades);
-  print("Carla");
+void CelsiusParaFahrenhed(){
+  stdout.write("Digite a temperatura em Celsius: ");
+  double celsius = double.parse(stdin.readLineSync()!);
+  print("Resultado: $celsius°C = ${((celsius * 9/5) + 32)}");
+}
+void FahrenheitParaCelsius(){
+  stdout.write("Digite a temperatura em Fahrenheit");
+  double fahrenheit = double.parse(stdin.readLineSync()!);
+  print("Resultado: $fahrenheit°C =${((fahrenheit - 32) * 9/5)}");
+}
+void CelsiusParaKelvin(){
+  stdout.write("Digite a temperatura em Celsius");
+  double celsius = double.parse(stdin.readLineSync()!);
+  print("Resultado: $celsius°C = ${celsius + 273.15}");
+}
+void KelvinParaCelsius(){
+  stdout.write("Digite a temperatura de Kelvin");
+  double kelvin = double.parse(stdin.readLineSync()!);
+  print("Resultado: $kelvin°C =${kelvin + 273.15}");
+}
+void main(){
+  int opcaoSair = 0;
+  do{
+    print('Escolha uma opção:');
+    print("==== Conversões de temperatra");
+    print("1, Celsius para Fahrenheit");
+    print("2, Fahrenheit para Celsius");
+    print("3, Celsius para kelvin");
+    print("4, kelvin para Celsius");
+    print("Sair");
+    try{
+      stdout.write(("Escolha uma opção"));
+      int opcao = int.parse(stdin.readLineSync()!);
+      switch(opcao){
+        case 1: CelsiusParaFahrenhed(); break;
+        case 2: FahrenheitParaCelsius(); break;
+        case 3: CelsiusParaKelvin(); break;
+        case 4: KelvinParaCelsius(); break;
+        case 5: exit(1);
+      }
+      print("Confirme a sua saída? 0 - sim / 1 - Não");
+      opcaoSair = int.parse(stdin.readLineSync()!);
+    }catch(e){
+      print("Entrada errada");
+  }
+ }while(opcaoSair != 0);
 }
